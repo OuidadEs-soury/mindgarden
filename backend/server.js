af8fb9940f57a7fe6c013db71771375ef3089f43
Ouidad7 <widadelsouri@gmail.com>
2026-04-26 23:02:47 +0100

    User

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// 🔗 CONNECT TO DATABASE
 mongoose.connect("mongodb://127.0.0.1:27017/mindgarden")
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-// Routes
-app.use("/habits", require("./routes/habits"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/habits", require("./routes/habits"));
 
 app.listen(5000, () => {
   console.log("Server running on http://localhost:5000");
