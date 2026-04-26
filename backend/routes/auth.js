dbc8ca6ab9d00c870477383f1b939f92f8d70693
Ouidad7 <widadelsouri@gmail.com>
2026-04-26 23:04:19 +0100

    USER AUTH

---
+++ backend/routes/auth.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  const hashed = await bcrypt.hash(req.body.password, 10);
+
+  const user = new User({
+    email: req.body.email,
+    password: hashed
+  });
+
+  await user.save();
+  res.json({ msg: "User created" });
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).json({ msg: "User not found" });
+
+  const valid = await bcrypt.compare(req.body.password, user.password);
+  if (!valid) return res.status(400).json({ msg: "Wrong password" });
+
+  const token = jwt.sign({ id: user._id }, "secret123");
+  res.json({ token });
+});
+
+module.exports = router;
