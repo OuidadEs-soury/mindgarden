aed75c854e12d4a28e7222c5bd4a3ebf81c3fdc6
Ouidad7 <widadelsouri@gmail.com>
2026-04-26 23:03:23 +0100

    HabitSchema

--- backend/models/Habit.js
+++ backend/models/Habit.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const HabitSchema = new mongoose.Schema({
+  userId: String,
   name: String,
   streak: { type: Number, default: 0 },
   lastDone: { type: String, default: null }
