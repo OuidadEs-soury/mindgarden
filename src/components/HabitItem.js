a8e27eacff654c412db90c5abd0829f877809879
Ouidad7 <widadelsouri@gmail.com>
2026-04-26 23:11:05 +0100

    Item

--- src/components/HabitItem.js
+++ src/components/HabitItem.js
@@ -1,18 +1,33 @@
 import React from "react";
 
-export default function HabitItem({ habit, onComplete, onDelete }) {
+export default function HabitItem({ habit, token }) {
+
+  const complete = async () => {
+    await fetch(`http://localhost:5000/api/habits/${habit._id}`, {
+      method: "PUT",
+      headers: { authorization: token }
+    });
+    window.location.reload();
+  };
+
+  const remove = async () => {
+    await fetch(`http://localhost:5000/api/habits/${habit._id}`, {
+      method: "DELETE",
+      headers: { authorization: token }
+    });
+    window.location.reload();
+  };
+
   return (
     <div className="card">
-      <div className="info">
+      <div>
         <h3>{habit.name}</h3>
-        <p>🔥 Streak: {habit.streak}</p>
+        <p>🔥 {habit.streak}</p>
       </div>
 
-      <div className="actions">
-        <button onClick={() => onComplete(habit.id)}>Done</button>
-        <button className="delete" onClick={() => onDelete(habit.id)}>
-          ✕
-        </button>
+      <div>
+        <button onClick={complete}>Done</button>
+        <button onClick={remove}>Delete</button>
       </div>
     </div>
   );
