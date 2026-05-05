921d66cd438ce08c9f04cea09762c16403317e87
Ouidad7 <widadelsouri@gmail.com>
2026-05-05 15:58:02 +0100

    server up

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,11 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/habits", require("./routes/habits"));
 
-// Connect DB
+// MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/mindgarden")
-.then(() => console.log("MongoDB connected"))
+.then(() => console.log("✅ MongoDB connected"))
 .catch(err => console.log(err));
 
 app.listen(5000, () => {
-  console.log("Server running on port 5000");
+  console.log("🚀 Server running on port 5000");
 });
